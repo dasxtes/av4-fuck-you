68cc77ccf2ec17c92264ef047a898d6eaf05f9ad
dasxtes <140609752+dasxtes@users.noreply.github.com>
2024-10-29 08:46:00 -0300

    efdef

---
+++ perguntas.js
@@ -0,0 +1,56 @@
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'A fórmula é base X altura dividido por 2',
+)
+criaCartao(
+    'Português',
+    'Qual é o sujeito da frase "O cachorro corre no parque"?',
+    'O Cachorro',
+)
+criaCartao(
+    'Ingles',
+    'ual é o significado da palavra "breeze" em português?',
+    'Brisa',
+)
+criaCartao(
+    'História',
+    'Quem foi o imperador romano que dividiu o Império em duas partes?',
+    'O imperador Diocleciano.',
+)
+criaCartao(
+    'Sociologia',
+    'O que é estratificação social?',
+    'divisão da sociedade em camadas',
+)
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo em termos de área?',
+    'A Ásia',
+)
+criaCartao(
+    'Educação Financeira',
+    'O que é um orçamento pessoal?',
+    'É um planejamento das receitas e despesas de uma pessoa.',
+)
+criaCartao(
+    'Quimica',
+    'Qual é o pH de uma solução neutra?',
+    '7',
+)
+criaCartao(
+    'Programação',
+    'Qual tag é usada para representar o cabeçario? ',
+    'header',
+)
+criaCartao(
+    'Biologia',
+    'Qual é a função principal das mitocôndrias nas células? ',
+    'As mitocôndrias geram energia na célula. ',
+)
+criaCartao(
+    'Física',
+    'O que é a Lei de Newton da Gravitação Universal? ',
+    'Todos os corpos se atraem com uma força que depende das suas massas e da distância entre eles.',
+)
+
