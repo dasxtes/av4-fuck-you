f4cd4fa69e560ff63de954221cc1ccbc07dd885f
dasxtes <140609752+dasxtes@users.noreply.github.com>
2024-11-19 08:04:50 -0300

    ggg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
     cartao.innerHTML = `
-<div class="cartao__conteudo">
+                <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
                         <p>${pergunta} </p>
@@ -14,11 +14,11 @@ function criaCartao(categoria, pergunta, resposta) {
                 </div>
                 `
     let respostaVisivel = false
-    function viraCartao(){
+    function viraCartao() {
         respostaVisivel = !respostaVisivel
-        cartao.classList.toggle('active',respostavisivel)
+        cartao.classList.toggle('active', respostavisivel)
     }
-cartao.addEventListener ('click', viraCartao)
-let container = document.getElementById('container')
-container.appendChild(cartao)
+    cartao.addEventListener('click', viraCartao)
+    let container = document.getElementById('container')
+    container.appendChild(cartao)
 }
