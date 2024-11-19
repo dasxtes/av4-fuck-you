90ff4d7ff314190576011282c7a1e524822b3d29
dasxtes <140609752+dasxtes@users.noreply.github.com>
2024-11-19 07:26:07 -0300

    efwf

--- perguntas.js
+++ perguntas.js
@@ -1,42 +1,42 @@
 criaCartao(
     'Matemática',
     'Qual é a fórmula para calcular a área de um triângulo?',
-    'A fórmula é base X altura dividido por 2',
+    'A fórmula é base X altura dividido por 2'
 )
 criaCartao(
     'Português',
     'Qual é o sujeito da frase "O cachorro corre no parque"?',
-    'O Cachorro',
+    'O Cachorro'
 )
 criaCartao(
     'Ingles',
     'ual é o significado da palavra "breeze" em português?',
-    'Brisa',
+    'Brisa'
 )
 criaCartao(
     'História',
     'Quem foi o imperador romano que dividiu o Império em duas partes?',
-    'O imperador Diocleciano.',
+    'O imperador Diocleciano.'
 )
 criaCartao(
     'Sociologia',
     'O que é estratificação social?',
-    'divisão da sociedade em camadas',
+    'divisão da sociedade em camadas'
 )
 criaCartao(
     'Geografia',
     'Qual é o maior continente do mundo em termos de área?',
-    'A Ásia',
+    'A Ásia'
 )
 criaCartao(
     'Educação Financeira',
     'O que é um orçamento pessoal?',
-    'É um planejamento das receitas e despesas de uma pessoa.',
+    'É um planejamento das receitas e despesas de uma pessoa.'
 )
 criaCartao(
     'Quimica',
     'Qual é o pH de uma solução neutra?',
-    '7',
+    '7'
 )
 criaCartao(
     'Programação',
@@ -46,11 +46,11 @@ criaCartao(
 criaCartao(
     'Biologia',
     'Qual é a função principal das mitocôndrias nas células? ',
-    'As mitocôndrias geram energia na célula. ',
+    'As mitocôndrias geram energia na célula. '
 )
 criaCartao(
     'Física',
     'O que é a Lei de Newton da Gravitação Universal? ',
-    'Todos os corpos se atraem com uma força que depende das suas massas e da distância entre eles.',
+    'Todos os corpos se atraem com uma força que depende das suas massas e da distância entre eles.'
 )
 
