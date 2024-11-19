7ff230945c46f017132ad50a8d12ce15d99044b9
dasxtes <140609752+dasxtes@users.noreply.github.com>
2024-11-19 07:27:48 -0300

    ewfwe

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function criaCartao(categoria, pergunta, resposta) {
-let cartao = document.createElement ('article')
-cartao.className = 'cartao'
-cartao.innerHTML = `
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
 <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
@@ -11,8 +11,9 @@ cartao.innerHTML = `
                         <p>${resposta}
                         </p>
                     </div>
-                </div>`
+                </div>
+                `
 
-    let container = document.getElementById ('container')
-    container.appendChild (cartao)
+    let container = document.getElementById('container')
+    container.appendChild(cartao)
 }
